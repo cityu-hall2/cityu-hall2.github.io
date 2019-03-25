75e0e03ba1374ee334d812ba930e25448b2094ef
Pengze Liu <lpzjerry@gmail.com>
2019-03-25 12:26:33 +0800

    remove drive

--- drive/version.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$OC_Version = array(9,1,4,2);
-$OC_VersionString = '9.1.4';
-$OC_Edition = '';
-$OC_Channel = 'stable';
-$OC_VersionCanBeUpgradedFrom = array(9,0);
-$OC_Build = '2017-02-01T18:44:48+00:00 c2cb190b880ce9e7992801204de0fb2cda988d4f';
