468efdf5072cba391061a2156fdb7d7dc138a973
ryan yen <45389924+ryanyen2@users.noreply.github.com>
2019-10-04 21:19:28 +0800

    Update main.js

--- cityhack/cityhack gallery/js/main.js
+++ cityhack/cityhack gallery/js/main.js
@@ -10,23 +10,7 @@ jQuery(document).ready(function($) {
 
 	
 
-	var scrollable = true;
 
-	var listener = function(e) {
-	    if (! scrollable) {
-		e.preventDefault();
-	    }
-	}
-
-	document.addEventListener('touchmove', listener, { passive:false });
-
-	dragula([dndContainer], {
-	    direction: 'horizontal'
-	}).on('drag', function(el, source) {
-	    scrollable = false;
-	}).on('drop', function(el, source) {
-	    scrollable = true;
-	});
 
 	
 	var siteMenuClone = function() {
